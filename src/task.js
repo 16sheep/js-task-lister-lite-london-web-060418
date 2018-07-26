5e7ed83b182271e13aeda8907e0aae2d4f904e3a
github username <github email address>
2018-07-26 21:29:37 +0100

    DONE

--- src/task.js
+++ src/task.js
@@ -1,2 +1,36 @@
+let listArr =  []
+const submit = document.querySelectorAll('input')[1]
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    new Task(document.querySelectorAll('input')[0].value)
+})
+
 class Task {
+  constructor(name = name) {
+    this.name = name
+    listArr.push(this)
+    renderTask()
+  }
+}
+
+const renderTask = () => {
+  let li = document.createElement('li')
+  let val = document.getElementById('new-task-description').value
+  li.innerText = val
+  if (document.getElementById('high').selected === true){
+    li.style.color = "red"
+  }
+  else if (document.getElementById('medium').selected === true){
+    li.style.color = "yellow"
+  }
+  else if (document.getElementById('low').selected === true){
+    li.style.color = "green"
+  }
+  let del = document.createElement('button')
+  del.innerText = "Delete Task"
+  li.append(del)
+  del.addEventListener("click", function (e){
+    e.target.parentNode.parentNode.removeChild(li)
+  })
+  taskContainer.append(li)
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,13 @@
+const taskContainer = document.getElementById('tasks')
+// const input = document.querySelectorAll('input')[0].value
+// const submit = document.querySelectorAll('input')[1]
+
+
 class TaskList {
+  addLastTask() {
+    let lastTask = listArr[-1]
+    lastTask = document.createElement('li')
+    taskContainer.append(lastTask)
+  }
+
 }
